16b8fd0b65e6355f1c7d56e56a628d6200c2b42d
rakesh <rakesh.haridas@tcs.com>
2021-12-13 16:08:10 +0000

    adding python files

---
+++ calculator.py
@@ -0,0 +1,6 @@
+print("enter a number between 1 and 35")
+number=input()
+for(number <=20)
+    print(number)
+    number+=1
+
---
+++ hello.py
@@ -0,0 +1,4 @@
+print("hello world")
+age=input("what's your age?")
+print("your age is",age)
+
